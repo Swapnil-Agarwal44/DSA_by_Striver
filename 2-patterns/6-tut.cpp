821c78f1e46c76829e1381774773f40ca666dc7d
Swapnil Agarwal <agarwalswapnil222@gmail.com>
2025-05-23 15:02:30 +0530

    another pattern

--- 2-patterns/6-tut.cpp
+++ 2-patterns/6-tut.cpp
@@ -20,17 +20,12 @@ for(int i=1; i<=n; i++){
 }
 
 void pattern2(int n){
-    for(int i=1; i<=4; i++){
-        for(int j=1;j<=i; j++){
-            cout<<j;
-        }
-
-        for(int j=1; j<=2*(n-i); j++){
+    int a=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<a;
+            a++;
             cout<<" ";
-        }
-
-        for(int j=i; j>=1; j--){
-            cout<<j;
         }
         cout<<endl;
     }
